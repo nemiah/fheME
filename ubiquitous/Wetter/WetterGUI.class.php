a7a1b51f01284ff45b1b140095b70db42d9be952
RicoTetz <RicoTetz@gmx.de>
2012-07-17 05:34:20 +0200

    Link für WOEID hinzugefügt

--- ubiquitous/Wetter/WetterGUI.class.php
+++ ubiquitous/Wetter/WetterGUI.class.php
@@ -14,22 +14,22 @@
 
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *  2007 - 2012, Rainer Furtmeier - Rainer@Furtmeier.de
  */
 class WetterGUI extends Wetter implements iGUIHTML2 {
 	function getHTML($id){
 		$gui = new HTMLGUIX($this);
 		$gui->name("Wetter");
-	
+
 		$B = $gui->addSideButton("Daten\nanzeigen", "empty");
 		$B->popup("", "Wetter-Daten", "Wetter", $this->getID(), "getData");
-		
-		#$gui->descriptionField("WetterWOEID", "Siehe <a href=\"http://isithackday.com/geoplanet-explorer/\" target=\"_blank\">http://isithackday.com/geoplanet-explorer/</a>");
-		
+
+		$gui->descriptionField("WetterWOEID", "Siehe: <a href=\"http://edg3.co.uk/snippets/weather-location-codes/\" target=\"_blank\">http://edg3.co.uk/snippets/weather-location-codes/</a>");
+
 		return $gui->getEditHTML();
 	}
-	
+
 	public function getData() {
 		echo "<pre style=\"height:400px;overflow:auto;\">";
 		print_r(parent::getData());
