aae93860b2829a73a541c816b5923c9d956e8954
Nena Furtmeier <Nena@Furtmeier.IT>
2023-05-12 14:55:01 +0200

    fix

--- fheME/Wetterstation/mWetterstationGUI.class.php
+++ fheME/Wetterstation/mWetterstationGUI.class.php
@@ -33,7 +33,64 @@ class mWetterstationGUI extends anyC implements iGUIHTMLMP2 {
 		
 		return $gui->getBrowserHTML($id);
 	}
+	public function getOverviewContent($echo = true){
+		$html = "<div class=\"touchHeader\"><span class=\"lastUpdate\" id=\"lastUpdatemWetterstationGUI\"></span><p>Wetter</p></div><div style=\"padding:10px;height:249px;overflow:auto;\">";
+		while($W = $this->n()){
+			#$W->download();
+			
+			$html .= "<div style=\"margin-top:5px;\">
+				<b style=\"font-size:30px;font-weight:bold;color:#555;\">".$W->A("WetterstationOutdoorTemp")." °C</b>
+				</div>";
+			
+			$sunInfo = date_sun_info(time(), $W->A("WetterstationLat"), $W->A("WetterstationLon"));
+			
+			$BSR = new Button("Sonnenaufgang", "./fheME/Wetterstation/sunrise.svg", "icon");
+			$BSR->style("height:15px;margin-right:3px;vertical-align:bottom;");
+			
+			$BSS = new Button("Sonnenuntergang", "./fheME/Wetterstation/sunset.svg", "icon");
+			$BSS->style("height:15px;margin-right:3px;vertical-align:bottom;");
+			
+			$html .= "<div style=\"margin-top:15px;\">$BSR ".date("H:i", $sunInfo["sunrise"])." $BSS ".date("H:i", $sunInfo["sunset"])."</div>";
+			
+			$MRS = new MoonRiseSet();
+			$moonInfo = $MRS->calculateMoonTimes(date("m"), date("d"), date("Y"), $W->A("WetterstationLat"), $W->A("WetterstationLon"));
+			
+			$BMR = new Button("Mondaufgang", "./fheME/Wetterstation/moonrise.svg", "icon");
+			$BMR->style("height:15px;margin-right:3px;vertical-align:bottom;");
+			
+			$BMS = new Button("Monduntergang", "./fheME/Wetterstation/moonset.svg", "icon");
+			$BMS->style("height:15px;margin-right:3px;vertical-align:bottom;");
+			
+			
+			$MP = new MoonPhase();
+			
+			$html .= "
+				<div style=\"margin-top:15px;\">
+					<div style=\"float:left;margin-right:10px;font-size:3em;margin-right:15px;margin-left:10px;\">".$MP->getPhaseEmoji()."</div>
+					<div style=\"\">".$MP->getPhaseName()." <span style=\"color:grey;\">".round($MP->getIllumination() * 100)."%</span><br><small style=\"color:grey;\">🌕 ".Util::CLDateParser($MP->getNextFullMoon())."<br>🌑 ".Util::CLDateParser($MP->getNextNewMoon())."</small></div>
+				</div>";
+			
+			$html .= "<div style=\"margin-top:15px;\">$BMR ".date("H:i", $moonInfo->moonrise)." $BMS ".date("H:i", $moonInfo->moonset)."</div>";
+			
+		}
+		
+		
+		
+		$html .= "</div>";
+		
+		
+		if($echo)
+			echo $html;
+		return $html;
+	}
 
+	
+	public static function getOverviewPlugin(){
+		$P = new overviewPlugin("mWetterstationGUI", "Wetterstation", 210);
+		$P->updateInterval(1800);
+		
+		return $P;
+	}
 
 }
 ?>
