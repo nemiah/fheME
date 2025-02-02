46b5e1c5bba68e923ede36ff1c0d51ef3266c399
Nena Furtmeier <Nena@Furtmeier.IT>
2025-02-02 14:10:40 +0100

    fix

--- fheME/Heizung/Heizung.class.php
+++ fheME/Heizung/Heizung.class.php
@@ -111,7 +111,7 @@ class Heizung extends PersistentObject {
 		return $parsed;
 	}
 	
-	public static $raumtempDefault = 21;
+	public static $raumtempDefault = 18;#21;
 	public static $raumtempHeat = 28;
 	public static $summerModeTempDefault = 13;
 	public static $summerModeTempHeat = 20;
